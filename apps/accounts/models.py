06063daef8c614ef0c6af2f2ecf90da18112e0c4
Hristo Velichkov <xristo.velichkov@gmail.com>
2023-07-29 09:27:54 +0300

    Make first_name, last_name, email required

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -5,12 +5,17 @@ from django.urls import reverse_lazy
 
 
 class AccountUser(AbstractUser):
+    first_name = models.CharField("first name", max_length=150)
+    last_name = models.CharField("last name", max_length=150)
+    email = models.EmailField("email address", unique=True)
+
+
     def unread_notifications(self):
         return self.notifications.filter(is_read=False)
 
     def unread_notificaitons_count(self):
         return self.unread_notifications().count()
-    
+
 
 UserModel = get_user_model()
 
