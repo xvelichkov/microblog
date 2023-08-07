c22346659c96c31e9e4a10731d18c4acba0be569
Hristo Velichkov <xristo.velichkov@gmail.com>
2023-08-07 21:18:09 +0300

    Use PostgreSQL

--- microblog/settings.py
+++ microblog/settings.py
@@ -86,9 +86,13 @@ WSGI_APPLICATION = 'microblog.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "microblog",
+        "USER": "postgres",
+        "PASSWORD": "Passw0rd1",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
