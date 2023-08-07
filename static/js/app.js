3ba00175550db8f685b049c473b4571e2fc943e5
Hristo Velichkov <xristo.velichkov@gmail.com>
2023-08-07 09:59:00 +0300

    Move js and custom styles to separate files

---
+++ static/js/app.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function () {
+    $('.spinner-border').hide();
+    var infinite = new Waypoint.Infinite({
+        element: $('.infinite-container')[0],
+        handler: function (direction) {
+            console.log(diretion);
+
+        },
+        offset: 'bottom-in-view',
+
+        onBeforePageLoad: function () {
+            $('.spinner-border').show();
+        },
+        onAfterPageLoad: function () {
+            jdenticon();
+            $('.spinner-border').hide();
+        }
+    });
+});
